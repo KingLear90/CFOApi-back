3e28c71ea3325442bed3ba14b61ec23f95512711
Alan S. <lic.sanchezalan@gmail.com>
2025-04-29 14:07:41 -0300

    authenticate midd

--- backend/src/routes/clientRoute.ts
+++ backend/src/routes/clientRoute.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import * as ClientController from '../controllers/client.controller';
 import isAdminAuth from '../middlewares/isAdminAuth';
+import { limiter } from '../utils/limiter';
+import { authenticate } from '../middlewares/auth';
 
 const router = express.Router();
 
-router.get('/', isAdminAuth, ClientController.getClients);
-router.get('/:id', ClientController.getClientById);
-router.post('/', isAdminAuth, ClientController.createClient);
-router.put<{id: string}>('/:id', isAdminAuth, ClientController.updateClient);
-router.delete('/:id', isAdminAuth, ClientController.deleteClient);
+router.use(limiter)
+
+router.get('/', authenticate, isAdminAuth, ClientController.getClients);
+router.get('/:id', authenticate, ClientController.getClientById);
+router.post('/', authenticate, isAdminAuth, ClientController.createClient);
+router.put<{id: string}>('/:id', authenticate, isAdminAuth, ClientController.updateClient);
+router.delete('/:id', authenticate, isAdminAuth, ClientController.deleteClient);
 
 export default router;
