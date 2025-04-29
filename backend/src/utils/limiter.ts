0104ae11ba87812c17bf74cb51683821ecf456aa
Alan S. <lic.sanchezalan@gmail.com>
2025-04-29 14:05:46 -0300

    limit from 3 to 4

--- backend/src/utils/limiter.ts
+++ backend/src/utils/limiter.ts
@@ -2,6 +2,6 @@ import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 minuto
-    limit: 3,
+    limit: 4,
     message: {"error": 'Too many requests, please try again later'},
 })
